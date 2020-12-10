bdf91019f1f5cf51a1193bbfcfc693bc62f01fa7
marvin <koester.marvin@me.com>
2020-12-10 13:04:33 +0100

    e2e-test update

--- e2e_test.sh
+++ e2e_test.sh
@@ -13,7 +13,7 @@ docker-compose -f ./e2e/docker-compose.yml up --build --exit-code-from cypress
 # check exit-code of docker-run
 if [ $? -eq 0 ]
 then
-  echo "${SUCCESS}Successfully ran tests. Everything ready to go${NOCOLOR}"
+  echo "${SUCCESS}Successfully ran tests. Everything's ready to go${NOCOLOR}"
   true #
 else
   echo "${FAILURE}Tests failed. See logs for detailed info${NOCOLOR}"
